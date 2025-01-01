fb62b3914ef2c278b867bb3ba93a659a0ed520f7
cat1525 <ryuukyou34@gmail.com>
2025-01-01 20:00:37 +0900

    なんか分からん

--- lib/timer.dart
+++ lib/timer.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
-
+import 'package:just_audio/just_audio.dart';
+import 'package:audio_session/audio_session.dart';
 import 'package:flutter/material.dart';
+
+
 
 class MYTIME extends StatelessWidget {
   const MYTIME({super.key});
@@ -19,9 +22,47 @@ class TimerBody extends StatefulWidget {
 }
 
 class _TimerBodyState extends State<TimerBody> {
+  //音声
+  late AudioPlayer _player;
+  
+
   int _timer_text =0;
   Timer? _timer;
   bool isPaused = false;
+  @override
+  void initState(){
+    super.initState();
+
+    _setupSession();
+  }
+
+  Future<void> _setupSession() async{
+    _player = AudioPlayer();
+    final session = await AudioSession.instance;
+    await session.configure(AudioSessionConfiguration.music());
+    await _loadAudioFile();
+
+  }
+
+  Future<void> _loadAudioFile() async{
+    try{
+      await _player.setAsset('assets\bleep-41488.mp3');
+
+    }catch(e){
+      print(e);
+
+    }
+  }
+
+  Future<void> _playSoundFile() async{
+    if(_player.processingState==ProcessingState.completed){
+      await _loadAudioFile();
+    }
+
+    await _player.setSpeed(1);
+    await _player.play();
+  }
+
 
   @override
   void dispose(){
@@ -34,9 +75,9 @@ class _TimerBodyState extends State<TimerBody> {
     return Scaffold(
       appBar: AppBar(title: Text("timer"),),
       body: Center(child: Column(children: [
-        Text("${_timer_text~/60}min,${_timer_text%60}sec",style: TextStyle(fontSize: 32),),
-        TextButton(onPressed: (){isPaused? pauseTime():resume();}, child: Text("${isPaused?"resume ":"start"}")),
-        TextButton(onPressed: ()=>setUpTime(), child: Text("STSTS"))
+        Text("${_timer_text~/60}min,${_timer_text%60}sec",style: TextStyle(fontSize: 32*2),),
+        TextButton(onPressed: (){isPaused? pauseTime():resume();}, child: Text("${isPaused?"resume ":"REstart"}")),
+        TextButton(onPressed: ()=>setUpTime(), child: Text("START"))
       ],),),
     );
     
@@ -45,18 +86,31 @@ class _TimerBodyState extends State<TimerBody> {
     setState(() {
       isPaused=false;
     });
-    _timer_text=60*25;
+    _timer_text=25;
     _timer?.cancel();
     
     _timer=Timer.periodic(
       Duration(seconds: 1), 
-      (Timer time){
+      (Timer time) async {
         if(_timer_text>0 && !isPaused)
         setState(() {
           _timer_text--;
         });
         else if(_timer_text==0 ){
           time.cancel();
+          await _player.play();
+
+
+          showDialog(
+            context: context,
+            builder: (BuildContext context)=>AlertDialog(
+              title: Text("alert"),
+              content: Text("消して"),
+                actions: <Widget>[
+                  TextButton(onPressed: ()=> Navigator.pop(context,"OK"), child: Text("OK"))
+                ],
+            )
+          );
         }
         
       }
